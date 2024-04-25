7511ac725b82ab0fef30076ce25db6ca72043784
christian lozano <christian-lozano>
2024-04-25 10:59:17 -0500

    add testing

--- components/pdf/MainPdf.js
+++ components/pdf/MainPdf.js
@@ -1,49 +1,56 @@
 "use client";
 
 import PDF from "./Pdf";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { useEffect, useState } from "react";
 import { useCart } from "react-use-cart";
 
 export default function MainPdf({ catalogo }) {
   const { items } = useCart();
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   return (
     <>
       <div>
-        <PDFDownloadLink
-          document={<PDF catalogo={catalogo} items={items} />}
-          fileName={`${catalogo.name}.pdf`}
-        >
-          {({ loading, url, error, blob }) =>
-            loading ? (
-              <div className="fixed top-0 w-[100vw] bg-white/70 h-[100vh] z-[999] flex justify-center items-center">
-                <div class="flex items-center justify-center w-full h-full">
-                  <div class="flex justify-center items-center space-x-1 text-sm text-gray-700">
-                    <svg
-                      fill="none"
-                      class="w-6 h-6 animate-spin"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        clip-rule="evenodd"
-                        d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
-                        fill="currentColor"
-                        fill-rule="evenodd"
-                      />
-                    </svg>
+        {isClient && (
+          <PDFDownloadLink
+            document={<PDF catalogo={catalogo} items={items} />}
+            fileName={`${catalogo.name}.pdf`}
+          >
+            {({ loading, url, error, blob }) =>
+              loading ? (
+                <div className="fixed top-0 w-[100vw] bg-white/70 h-[100vh] z-[999] flex justify-center items-center">
+                  <div class="flex items-center justify-center w-full h-full">
+                    <div class="flex justify-center items-center space-x-1 text-sm text-gray-700">
+                      <svg
+                        fill="none"
+                        class="w-6 h-6 animate-spin"
+                        viewBox="0 0 32 32"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          clip-rule="evenodd"
+                          d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                        />
+                      </svg>
 
-                    <div>Generando PDF ...</div>
+                      <div>Generando PDF ...</div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <div className="h-[50vh] w-[100vw] flex justify-center items-center">
-                <button>Descargar! </button>
-              </div>
-            )
-          }
-        </PDFDownloadLink>
+              ) : (
+                <div className="h-[50vh] w-[100vw] flex justify-center items-center">
+                  <button>Descargar! </button>
+                </div>
+              )
+            }
+          </PDFDownloadLink>
+        )}
 
         {/* <PDFViewer>s
           <PDF catalogo={catalogo} items={items} />
